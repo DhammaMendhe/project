95b7c307ecd7d0632a1c2ea4d5a2cd4c26577c22
DhammaMendhe <send2dhammadipmendhe@gmail.com>
2024-01-11 13:16:55 +0530

    initial commit

---
+++ login.js
@@ -0,0 +1,9 @@
+// var submitButton = document.querySelector("button");
+
+
+
+// submitButton.addEventListener("click",myfunction()
+// )
+// function myfunction(){
+//     alert("first enter your email first")
+// }
